c439aa260effbc6bb50a9631b1b6a41057c8ad9c
Yannick Scherer <yannick.scherer@gmail.com>
2013-08-22 18:14:51 +0200

    Included Midje.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/xsc/lein-testem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.1.1"]]}}
   :eval-in-leiningen true)

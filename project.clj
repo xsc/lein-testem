3cac71ebe98627219c15d30d23a192de56d3ce3d
Yannick Scherer <yannick.scherer@gmail.com>
2013-09-08 11:48:02 +0200

    Back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-testem "0.1.0-alpha2"
+(defproject lein-testem "0.1.0-SNAPSHOT"
   :description "Minimal-Configuration Testing for Clojure."
   :url "https://github.com/xsc/lein-testem"
   :license {:name "Eclipse Public License"
